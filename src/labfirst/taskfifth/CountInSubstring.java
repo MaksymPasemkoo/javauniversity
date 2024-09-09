d55aadd8ed1fa3bb93ebd059c0ad3f65b29570bb
Max <pasemkomax@gmail.com>
2024-09-09 21:34:19 +0300

    6

--- src/labfirst/taskfifth/CountInSubstring.java
+++ src/labfirst/taskfifth/CountInSubstring.java
@@ -8,10 +8,10 @@ public class CountInSubstring {
         Scanner scanner = new Scanner(System.in);
 
         String[][] matrix = {
-                {"0", "1","0q","1"},
-                {"0", "1","0qq","1"},
-                {"0", "1","0","1"},
-                {"0", "1","0","1"}
+                {"0", "1", "1qq", "1"},
+                {"0", "1", "1qq123qq234q21qqqq", "1"},
+                {"0", "1", "0", "1"},
+                {"0", "1", "0", "1"}
         };
 
         System.out.print("Enter substring to search: ");
@@ -25,23 +25,23 @@ public class CountInSubstring {
         }
     }
 
-    private static int countSubstringInMatrix(final String[][] matrix,final String substring) {
+    private static int countSubstringInMatrix(final String[][] matrix, final String substring) {
         if (matrix == null || substring == null || substring.isEmpty()) {
             throw new IllegalArgumentException("Matrix or substring cannot be null or empty.");
         }
 
         int count = 0;
+        int fromIndex = 0;
         for (String[] row : matrix) {
             for (String element : row) {
-                for (int i = 0; i <= element.length() - substring.length(); i++) {
-                    if (element.startsWith(substring, i)) {
-                        count++;
-                    }
+                while ((fromIndex = element.indexOf(substring, fromIndex)) != -1) {
+                    count++;
+                    fromIndex += substring.length();
                 }
             }
         }
         return count;
     }
-
-
 }
+
+
